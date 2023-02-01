4bf143e398f70465c3bdeeb89841ac7dff798f18
Yaspeace <mikle0201@mail.ru>
2023-02-02 01:34:31 +0300

    Добавил axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = axios.create({
+  baseURL: "http://w980379k.beget.tech/index.php",
+});
 
 new Vue({
   router,
