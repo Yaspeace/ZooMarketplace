58769b155e8777d7056e085229df56a46a522d8d
podisodi <imogikyan@bk.ru>
2023-02-12 21:16:20 +0300

    красивое для бизьнеса

--- src/components/Profile/Offer.vue
+++ src/components/Profile/Offer.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="offer">
-    Продвижение объявлений 
+    <div class="offer-text">Вы владелец питомника, приюта или фермерского подворья?</div>
+    <div class="offer-lower">
+      Вам однозначно сюда: <br />
+      <a href="#"> Перейти в бизнес-аккаунт </a>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .offer {
   background: var(--secondary-grad);
+  font-weight: bold;
+  font-size: 18px;
+  text-align: left;
+  padding: 9px;
+}
+.offer-text{
+  font-size: 18px;
+  font-style: italic;
+}
+
+.offer-lower {
+  margin-top: 1.5em;
+}
+
+a {
+  text-decoration: underline;
+  font-size: 17px;
+  color:#f80657;
 }
 </style>
